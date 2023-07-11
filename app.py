efa3ff29a48e00c02b9ead3edc98945ce2df7720
AriManjikian <arikev2007@gmail.com>
2023-07-11 13:37:55 -0700

    pyrebase

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, render_template, request
+import pyrebase
+
+config = {
+  "apiKey": "AIzaSyB_4HVA1MMQWi56kUgrzwtsTrBh0itSa3o",
+  "authDomain": "accomplishr-78ffd.firebaseapp.com",
+  "projectId": "accomplishr-78ffd",
+  "storageBucket": "accomplishr-78ffd.appspot.com",
+  "messagingSenderId": "239633891989",
+  "appId": "1:239633891989:web:134eb1151d741675c7d19c",
+  "measurementId": "G-7QHRM0CMR6",
+  "databaseURL": "https://accomplishr-78ffd-default-rtdb.firebaseio.com/"
+}
+
 app = Flask(__name__)
 
+firebaseApp = pyrebase.initialize_app(config)
+database = firebaseApp.database()
 
 @app.route("/")
 def index():
