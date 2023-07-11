536d6831ba4e87a4b86f712b03e9f7d1c9e4aeed
AriManjikian <arikev2007@gmail.com>
2023-07-11 10:53:19 -0700

    test

--- run.js
+++ run.js
@@ -1,7 +1,5 @@
 const { spawn } = require('child_process');
-//const childPython = spawn ('python', ['--version']);
 const childPython = spawn ('python', ['../app.py']);
-// const child Python = spawn ('python', ['codespace.py', 'OyeKool']);
 childPython.stdout.on('data', (data) => {
 console.log(`stdout: ${data}`);
 });
@@ -11,19 +9,3 @@ childPython.stderr.on('data', (data) => {
 childPython.on('close', (code) =>{
   console.log(`child process exited with code ${code}`);
 });
-/*/
-const inputs = document.querySelectorAll('[data-status]')
-
-inputs.forEach(input =>{
-  input.addEventListener('click', (e)=>{
-    const status = input.dataset.status === 'unchecked' ? true : false
-    if(status){
-      inputs.forEach(input =>{
-        input.dataset.status = 'unchecked'
-      })
-      input.dataset.status = 'checked'
-    }else{
-      input.dataset.status = 'unchecked'
-    }
-  })
-})*/
