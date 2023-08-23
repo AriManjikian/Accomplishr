e443539955c010c9012a50a114440b5f3e38c55c
AriManjikian <arikev2007@gmail.com>
2023-08-22 22:12:27 -0700

    a

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/privacy-policy")
+def index():
+    return render_template("privacy-policy.html")
+
 
 
 if __name__ == "__main__":
