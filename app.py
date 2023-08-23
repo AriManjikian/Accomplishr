da3dd6a6858b045b7cc725fd8083cb0307b36504
AriManjikian <arikev2007@gmail.com>
2023-08-22 22:10:08 -0700

    a

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request
+import firebase_admin
+from firebase_admin import credentials, db, auth
 
 
 
 app = Flask(__name__)
+
+
+#FLASK
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
